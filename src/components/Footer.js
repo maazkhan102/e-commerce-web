ae8b38c3cfa06a1f12357887a65c09effb886566
maaz khan <maazkhan102@gmail.com>
2022-01-23 06:53:25 -0800

    aaaaaa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,33 +12,32 @@ const Footer = () => {
         {/* <div className={`footerhead  ${show && "moveup"}`}> */}
         <div className="footerhead">
           <div className="footerwrap">
-            <h4>Owner & Creator:</h4>
+            <h4>Owner & Creators:</h4>
             <span style={{ fontWeight: "700", fontSize: "25px" }}>
-              Hassan Ahmed Khan.
+              Abbas | Maaz | Ramis | Taha
             </span>
             <h5>
-              Copyright <span className="copyicon">©️</span> 2021 Hassan Ahmed
-              Khan | All Rights Reserved.
+              Copyright <span className="copyicon">©️</span> 2021 Muhammad Maaz Khan | All Rights Reserved.
             </h5>
           </div>
           <div className="socialicons">
-            <a href="https://www.facebook.com/theycallmekhan/" alt="FB Icon">
+            <a href="https://www.facebook.com/muhammad.maazkhan.925" alt="FB Icon">
               <img src={fb} className="icon" alt="img" />
             </a>
 
             <a
-              href="https://www.instagram.com/hassanahmedkhann/"
+              href="https://www.instagram.com/maazkhan____102/"
               alt="Insta Icon"
             >
               <img src={insta} className="icon" alt="img" />
             </a>
             <a
-              href="https://www.linkedin.com/in/hassan-ahmed-khan-937210202/"
+              href="https://www.linkedin.com/feed/"
               alt="LinkedIn Icon"
             >
               <img src={li} className="icon" alt="img" />
             </a>
-            <a href="mailto:itshakhere@gmail.com" alt="Email Icon">
+            <a href="https://mail.google.com/mail/u/0/#inbox" alt="Email Icon">
               <img src={em} className="icon" alt="img" />
             </a>
           </div>
